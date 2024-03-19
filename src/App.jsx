a08d12acc59213cc35628a5cb32aab58d964846c
somesh202 <somesh.ranjan.met20@itbhu.ac.in>
2024-03-19 15:27:37 +0530

    Debugging for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Table from './Table.jsx';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 import './App.css'
 function App () {
   return (
